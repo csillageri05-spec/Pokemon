19456c80f33d83e990057d425c0f4fd262115d3a
Csillag Gergő Zsolt (SZF_N_2025_2027) <csillag.gergo.zsolt@diak.szamalk-szalezi.hu>
2026-05-21 08:49:34 +0200

    stilus hozzaadva

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ import Services from "./Services.js";
 const taroloELEM = document.querySelector(".pokemon-tarolo");
 const adatELEM = document.querySelector(".adatok");
 
+
 const services = new Services();
 
-for (let i = 1; i <= 50; i++) {
+for (let i = 1; i <= 1028; i++) {
     services.getAdat(`https://pokeapi.co/api/v2/pokemon/${i}`, kepMegjelenit);
 }
 
 function kepMegjelenit(data) {
+ 
     new Pokemon(data, taroloELEM);
 }
 
